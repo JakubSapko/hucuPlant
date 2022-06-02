8bd79da098c624f821b770c3e76263ab90f3a3a7
JakubSapko <jakub.sapko1@gmail.com>
2022-06-02 19:27:24 +0200

    create Plant model

--- backend/base/models.py
+++ backend/base/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Plant(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    name = models.CharField(max_length=150)
+    date_added = models.DateField(auto_now_add=True)
+    description = models.TextField()
+    img = models.ImageField()
+    plant_species = models.CharField(max_length=150)
+    last_watered = models.IntegerField(default=0)
+    how_often = models.IntegerField(default=0)
