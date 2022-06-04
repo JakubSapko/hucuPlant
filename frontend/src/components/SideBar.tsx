ca4650c54fc08a476fa21c0e4feeb1a083406857
JakubSapko <jakub.sapko1@gmail.com>
2022-06-04 09:49:57 +0200

    add sidebar icons

--- frontend/src/components/SideBar.tsx
+++ frontend/src/components/SideBar.tsx
@@ -1,15 +1,25 @@
 import React from 'react';
+import {RiPlantFill} from "react-icons/ri";
+import {BsFillPieChartFill} from "react-icons/bs";
+import {MdGroups} from "react-icons/md";
 
+interface IIcon{
+    icon: JSX.Element;
+}
 const SideBar: React.FC = () => {
     return (
-    <div className='fixed top-0 left-0 h-screen w-32 m-0 flex flex-col bg-gray-900 text-white shadow-lg'>
-        <i>A</i>
-        <i>B</i>
-        <i>C</i>
-        <i>D</i>
-        <i>E</i>
+    <div className='fixed top-0 left-0 h-screen w-32 m-0 flex flex-col bg-primary text-contrast shadow-lg'>
+        <SideBarIcon icon={<RiPlantFill size="28"/>} />
+        <SideBarIcon icon={<BsFillPieChartFill size="28"/>}/>
+        <SideBarIcon icon={<MdGroups size="28"/>}/>
     </div>
-    )
-}
+    );
+};
+
+const SideBarIcon: React.FC<IIcon> = ({icon}) => {
+    return <div className="sidebar-icon">
+        {icon}
+    </div>
+};
 
 export default SideBar;
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,14 +3,14 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    colors: {
+      primary: '#1b6000', //dark green
+      secondary: '#679436', //light green
+      contrast: '#ed7d3a', //mango orange
+      content: '#f2e8cf', //egg white
+      darkish: '#000501' //dark
+    },
     extend: {
-      color: {
-        primary: '#1b6000', //dark green
-        secondary: '#679436', //light green
-        contrast: '#ed7d3a', //mango orange
-        content: '#f2e8cf', //egg white
-        darkish: '#000501' //dark
-      }
     },
   },
   plugins: [],
