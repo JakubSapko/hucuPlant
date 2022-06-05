073f47eb36be97c5f58729249a35e6a326a8922f
JakubSapko <jakub.sapko1@gmail.com>
2022-06-05 21:46:21 +0200

    add sidebar logout, add sidebar styling

--- frontend/src/components/SideBar.tsx
+++ frontend/src/components/SideBar.tsx
@@ -1,25 +1,38 @@
-import React from 'react';
-import {RiPlantFill} from "react-icons/ri";
-import {BsFillPieChartFill} from "react-icons/bs";
-import {MdGroups} from "react-icons/md";
+import React from "react";
+import { RiPlantFill } from "react-icons/ri";
+import { BsFillPieChartFill } from "react-icons/bs";
+import { MdGroups } from "react-icons/md";
+import { Button } from "antd";
 
-interface IIcon{
-    icon: JSX.Element;
+interface IIcon {
+  icon: JSX.Element;
+  text: string;
 }
 const SideBar: React.FC = () => {
-    return (
-    <div className='fixed top-0 left-0 h-screen w-32 m-0 flex flex-col bg-primary text-contrast shadow-lg'>
-        <SideBarIcon icon={<RiPlantFill size="28"/>} />
-        <SideBarIcon icon={<BsFillPieChartFill size="28"/>}/>
-        <SideBarIcon icon={<MdGroups size="28"/>}/>
+  return (
+    <div className="fixed top-0 left-0 h-screen w-32 m-0 flex flex-col bg-primary text-contrast shadow-lg">
+      <SideBarIcon
+        icon={<RiPlantFill size="28" />}
+        text="Check out your plants!"
+      />
+      <SideBarIcon icon={<BsFillPieChartFill size="28" />} text="Statistics" />
+      <SideBarIcon icon={<MdGroups size="28" />} text="Groups" />
+      <div className="pl-9 pt-10">
+        <Button ghost className="text-content hover:text-contrast">
+          Log Out
+        </Button>
+      </div>
     </div>
-    );
+  );
 };
 
-const SideBarIcon: React.FC<IIcon> = ({icon}) => {
-    return <div className="sidebar-icon">
-        {icon}
+const SideBarIcon: React.FC<IIcon> = ({ icon, text }) => {
+  return (
+    <div className="sidebar-icon group">
+      {icon}
+      <span className="sidebar-tooltip group-hover:scale-100">{text}</span>
     </div>
+  );
 };
 
 export default SideBar;
