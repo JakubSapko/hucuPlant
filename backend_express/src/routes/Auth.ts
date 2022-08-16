ce6c41f3812e2a03a6a6f262d73158f8ee0a6f3b
JakubSapko <jakub.sapko1@gmail.com>
2022-08-16 16:15:53 +0200

    add login

--- backend_express/src/routes/Auth.ts
+++ backend_express/src/routes/Auth.ts
@@ -6,40 +6,72 @@ export const auth = Router();
 const prisma = new PrismaClient();
 
 auth.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+
+  const user = await prisma.user.findUnique({
+    where: {
+      username: username,
+    },
+  });
+
+  if (!user) {
+    return res
+      .status(400)
+      .json({ success: false, error: "User of that name was not found" });
+  }
+
+  try {
+    if (password === user.password) {
+      const accessToken = jwt.sign({ userId: user.id }, "secret");
+      return res.status(200).json({ success: true, accessToken: accessToken });
+    } else {
+      return res
+        .status(400)
+        .json({ success: false, error: "Invalid password" });
+    }
+  } catch (err) {
+    return res
+      .status(400)
+      .json({ success: false, error: "Internal server errror" });
+  }
 });
 
 auth.post("/register", async (req, res) => {
-    const { email, username, password } = req.body;
+  const { email, username, password } = req.body;
 
-    const usernameResult = await prisma.user.findUnique({
-        where: {
-            username: username,
-        },
-    });
+  const usernameResult = await prisma.user.findUnique({
+    where: {
+      username: username,
+    },
+  });
 
-    if (usernameResult) {
-        return res.status(400).json({success: false, error: "User of that name already exists"})
-    }
+  if (usernameResult) {
+    return res
+      .status(400)
+      .json({ success: false, error: "User of that name already exists" });
+  }
 
-    const emailResult = await prisma.user.findUnique({
-        where: {
-            email: email,
-        },
-    });
+  const emailResult = await prisma.user.findUnique({
+    where: {
+      email: email,
+    },
+  });
 
-    if (emailResult) {
-        return res.status(400).json({success: false, error: "User with that email already exists"})
-    }
+  if (emailResult) {
+    return res
+      .status(400)
+      .json({ success: false, error: "User with that email already exists" });
+  }
 
-    const user = await prisma.user.create({
-        data: {
-            email: email,
-            username: username,
-            password: password
-        }
-    });
+  const user = await prisma.user.create({
+    data: {
+      email: email,
+      username: username,
+      password: password,
+    },
+  });
 
-    const accessToken = jwt.sign({userId: user.id}, "secret");
+  const accessToken = jwt.sign({ userId: user.id }, "secret");
 
-    return res.status(200).json({success: true, accessToken: accessToken});
+  return res.status(200).json({ success: true, accessToken: accessToken });
 });
