d41172eca177aa10af67c02a825733c2bb01c3ae
JakubSapko <jakub.sapko1@gmail.com>
2023-03-02 15:12:18 +0100

    add: routing on log in

--- client/src/hooks/auth/useLogin.ts
+++ client/src/hooks/auth/useLogin.ts
@@ -1,6 +1,8 @@
 import { useMutation } from "@tanstack/react-query"
 import { MessageInstance } from "antd/lib/message";
+import { useNavigate } from "react-router-dom";
 import { API, AuthKeys } from "../../services/api";
+import jwt_decode from "jwt-decode";
 
 export interface ILoginCredentials {
     email: string;
@@ -8,11 +10,14 @@ export interface ILoginCredentials {
 }
 
 export const useLogin = (messageApi: MessageInstance) => {
+    const navigate = useNavigate();
+
     const login = useMutation({
         mutationFn: (loginCredentials: ILoginCredentials) => 
             API.post(AuthKeys.LOGIN, loginCredentials),
         onSuccess: () => {
             messageApi.success("Welcome to our app!");
+            navigate("/home", {replace: true});
         },
         onError: () => {
             messageApi.error("You provided wrong credentials!")
