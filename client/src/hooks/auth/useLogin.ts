73f7041512da0eec35bb9e19ecb7e2c604365546
JakubSapko <jakub.sapko1@gmail.com>
2023-03-02 16:00:37 +0100

    change: logout is now hook based

--- client/src/hooks/auth/useLogin.ts
+++ client/src/hooks/auth/useLogin.ts
@@ -1,8 +1,9 @@
-import { useMutation } from "@tanstack/react-query"
+import { useMutation } from "@tanstack/react-query";
 import { MessageInstance } from "antd/lib/message";
 import { useNavigate } from "react-router-dom";
 import { API, AuthKeys } from "../../services/api";
 import jwt_decode from "jwt-decode";
+import { IAuthTokens, IUser, useAuthContext } from "../../context/AuthContext";
 
 export interface ILoginCredentials {
     email: string;
@@ -11,17 +12,25 @@ export interface ILoginCredentials {
 
 export const useLogin = (messageApi: MessageInstance) => {
     const navigate = useNavigate();
-
+    const { setAuthTokens, setUser } = useAuthContext();
     const login = useMutation({
-        mutationFn: (loginCredentials: ILoginCredentials) => 
-            API.post(AuthKeys.LOGIN, loginCredentials),
-        onSuccess: () => {
+        mutationFn: (loginCredentials: ILoginCredentials) =>
+            API.post<ILoginCredentials, IAuthTokens>(
+                AuthKeys.LOGIN,
+                loginCredentials
+            ),
+        onSuccess: (data) => {
+            setAuthTokens(data.data);
+            const decodedUser: IUser = jwt_decode(data.data.accessToken);
+            setUser(decodedUser);
+            localStorage.setItem("authTokens", JSON.stringify(data));
+
             messageApi.success("Welcome to our app!");
-            navigate("/home", {replace: true});
+            navigate("/home", { replace: true });
         },
         onError: () => {
-            messageApi.error("You provided wrong credentials!")
-        }
+            messageApi.error("You provided wrong credentials!");
+        },
     });
     return login;
-}
+};
