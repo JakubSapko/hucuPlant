746bcc662d3816ed2225dab50419273c4063a42c
JakubSapko <jakub.sapko1@gmail.com>
2023-06-11 14:43:21 +0200

    dump

--- client/src/pages/App/NewPlantSite.tsx
+++ client/src/pages/App/NewPlantSite.tsx
@@ -1,7 +1,8 @@
-import { LoadingOutlined, PlusOutlined } from "@ant-design/icons";
-import { Button, Form, Input, message, Upload } from "antd";
-import React, { useState } from "react";
+import { Button, Form, Input } from "antd";
+import React from "react";
 import styled from "styled-components";
+import { API } from "../../services/api";
+import { PlantKeys } from "../../services/api";
 
 const PlantFormContainer = styled.div`
     width: 100%;
@@ -21,91 +22,78 @@ const PlantForm = styled(Form)`
     flex-direction: column;
 `;
 
-const getBase64 = (img: any, callback: any) => {
-    const reader = new FileReader();
-    reader.addEventListener("load", () => callback(reader.result));
-    reader.readAsDataURL(img);
-};
+// const getBase64 = (img: any, callback: any) => {
+//     const reader = new FileReader();
+//     reader.addEventListener("load", () => callback(reader.result));
+//     reader.readAsDataURL(img);
+// };
 
-const beforeUpload = (file: any) => {
-    const isJpgOrPng = file.type === "image/jpeg" || file.type === "image/png";
+// const beforeUpload = (file: any) => {
+//     const isJpgOrPng = file.type === "image/jpeg" || file.type === "image/png";
 
-    if (!isJpgOrPng) {
-        message.error("You can only upload JPG/PNG file!");
-    }
+//     if (!isJpgOrPng) {
+//         message.error("You can only upload JPG/PNG file!");
+//     }
 
-    const isLt2M = file.size / 1024 / 1024 < 2;
+//     const isLt2M = file.size / 1024 / 1024 < 2;
 
-    if (!isLt2M) {
-        message.error("Image must smaller than 2MB!");
-    }
+//     if (!isLt2M) {
+//         message.error("Image must smaller than 2MB!");
+//     }
 
-    return isJpgOrPng && isLt2M;
-};
+//     return isJpgOrPng && isLt2M;
+// };
 
 export const NewPlantSite: React.FC = () => {
-    const [loading, setLoading] = useState(false);
-    const [imageUrl, setImageUrl] = useState<string>();
+    // const [loading, setLoading] = useState(false);
+    // const [imageUrl, setImageUrl] = useState<string>();
 
-    const handleChange = (info: any) => {
-        if (info.file.status === "uploading") {
-            setLoading(true);
-            return;
-        }
+    // const handleChange = (info: any) => {
+    //     if (info.file.status === "uploading") {
+    //         setLoading(true);
+    //         return;
+    //     }
 
-        if (info.file.status === "done") {
-            // Get this url from response in real world.
-            getBase64(info.file.originFileObj, (url: string) => {
-                setLoading(false);
-                setImageUrl(url);
-            });
-        }
+    //     if (info.file.status === "done") {
+    //         // Get this url from response in real world.
+    //         getBase64(info.file.originFileObj, (url: string) => {
+    //             setLoading(false);
+    //             setImageUrl(url);
+    //         });
+    //     }
+    // };
+
+    // const uploadButton = (
+    //     <div>
+    //         {loading ? <LoadingOutlined /> : <PlusOutlined />}
+    //         <div
+    //             style={{
+    //                 marginTop: 8,
+    //             }}
+    //         >
+    //             Upload
+    //         </div>
+    //     </div>
+    // );
+    const onFinish = (values: any) => {
+        const plant = {
+            name: values.name,
+            description: values.description,
+        };
+
+        API.post(PlantKeys.BASE, plant);
     };
-
-    const uploadButton = (
-        <div>
-            {loading ? <LoadingOutlined /> : <PlusOutlined />}
-            <div
-                style={{
-                    marginTop: 8,
-                }}
-            >
-                Upload
-            </div>
-        </div>
-    );
     return (
-        // <Upload
-        //   name="avatar"
-        //   listType="picture-card"
-        //   className="avatar-uploader"
-        //   showUploadList={false}
-        //   action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
-        //   beforeUpload={beforeUpload}
-        //   onChange={handleChange}
-        // >
-        //   {imageUrl ? (
-        //     <img
-        //       src={imageUrl}
-        //       alt="avatar"
-        //       style={{
-        //         width: '100%',
-        //       }}
-        //     />
-        //   ) : (
-        //     uploadButton
-        //   )}
-        // </Upload>
         <PlantFormContainer>
             <h1>Add a new plant to your collection!</h1>
-            <PlantForm>
+            <PlantForm onFinish={onFinish}>
                 <Form.Item label="Plant Name">
                     <Input placeholder="Plant Name" />
                 </Form.Item>
                 <Form.Item label="Plant Description">
                     <Input placeholder="Plant Description" />
                 </Form.Item>
-                <Form.Item label="Plant Image">
+                {/* <Form.Item label="Plant Image">
                     <Upload
                         name="avatar"
                         listType="picture-card"
@@ -125,7 +113,7 @@ export const NewPlantSite: React.FC = () => {
                             uploadButton
                         )}
                     </Upload>
-                </Form.Item>
+                </Form.Item> */}
                 <Form.Item>
                     <Button type="primary" htmlType="submit">
                         Add Plant
