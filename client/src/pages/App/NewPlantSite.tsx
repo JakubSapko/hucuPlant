47fd96e2acfeea84e3c287964b3d4abebb4bb63f
JakubSapko <jakub.sapko1@gmail.com>
2023-06-12 20:53:51 +0200

    remove: dead code

--- client/src/pages/App/NewPlantSite.tsx
+++ client/src/pages/App/NewPlantSite.tsx
@@ -26,62 +26,11 @@ const PlantForm = styled(Form<PlantFormValues>)`
     flex-direction: column;
 `;
 
-// const getBase64 = (img: any, callback: any) => {
-//     const reader = new FileReader();
-//     reader.addEventListener("load", () => callback(reader.result));
-//     reader.readAsDataURL(img);
-// };
-
-// const beforeUpload = (file: any) => {
-//     const isJpgOrPng = file.type === "image/jpeg" || file.type === "image/png";
-
-//     if (!isJpgOrPng) {
-//         message.error("You can only upload JPG/PNG file!");
-//     }
-
-//     const isLt2M = file.size / 1024 / 1024 < 2;
-
-//     if (!isLt2M) {
-//         message.error("Image must smaller than 2MB!");
-//     }
-
-//     return isJpgOrPng && isLt2M;
-// };
-
 export const NewPlantSite: React.FC = () => {
     const { user } = useAuthContext();
     const [api, contextHolder] = message.useMessage();
     const [isLoading, setIsLoading] = useState<boolean>(false);
-    // const [loading, setLoading] = useState(false);
-    // const [imageUrl, setImageUrl] = useState<string>();
 
-    // const handleChange = (info: any) => {
-    //     if (info.file.status === "uploading") {
-    //         setLoading(true);
-    //         return;
-    //     }
-
-    //     if (info.file.status === "done") {
-    //         // Get this url from response in real world.
-    //         getBase64(info.file.originFileObj, (url: string) => {
-    //             setLoading(false);
-    //             setImageUrl(url);
-    //         });
-    //     }
-    // };
-
-    // const uploadButton = (
-    //     <div>
-    //         {loading ? <LoadingOutlined /> : <PlusOutlined />}
-    //         <div
-    //             style={{
-    //                 marginTop: 8,
-    //             }}
-    //         >
-    //             Upload
-    //         </div>
-    //     </div>
-    // );
     const onFinish = (values: PlantFormValues) => {
         setIsLoading(true);
         const plantCreationPayload = {
